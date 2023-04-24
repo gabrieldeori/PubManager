bb0f6af6e76cbfa3465883da3a9219031c86aeb8
gabrieldeori <deori.pessoal@gmail.com>
2023-04-24 17:52:16 -0300

    feat: Adicionando BaseSelect

---
+++ frontend/src/components/BaseSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <label :for="'id_' + name">
+    {{ label }}
+    <select
+      :id="'id_' + name"
+      :name="name"
+      :value="modelValue"
+      v-bind="{
+        ...$attrs,
+        onChange: ($event) => { $emit('update:modelValue', $event.target.value) },
+      }"
+    >
+      <option
+        v-for="option in options"
+        :value="option"
+        :key="option"
+        :selected="option === modelValue"
+      >{{ option }}</option>
+    </select>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+
+</script>
