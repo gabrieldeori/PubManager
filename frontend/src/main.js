80cacc8c0f01a625776f79437c24eac9f2728257
gabrieldeori <deori.pessoal@gmail.com>
2023-04-24 15:56:04 -0300

    feat: Adicionando autoload de BaseComponents

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,25 @@
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/,
+);
+
+const app = createApp(App).use(router);
+
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1')),
+  );
+
+  app.component(componentName, componentConfig.default || componentConfig);
+});
+
+app.mount('#app');
