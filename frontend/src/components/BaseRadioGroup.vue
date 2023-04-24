0a72263899ea465dd9e60df6331a5378894f6c1c
gabrieldeori <deori.pessoal@gmail.com>
2023-04-24 19:27:50 -0300

    feat: Adicionando BaseRadioGroup componente

---
+++ frontend/src/components/BaseRadioGroup.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseRadio
+    v-for="{ value, label } in options"
+    :key="'key_' + value"
+    :value="value"
+    :label="label"
+    :name="name"
+    :modelValue="modelValue"
+    @update:modelValue="$emit('update:modelValue', $event)"
+   />
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: [String, Number, Boolean],
+      required: true,
+    },
+  },
+};
+</script>
