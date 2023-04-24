0cc0be9fd3c99bde6e9b4c79a60f46034396a924
gabrieldeori <deori.pessoal@gmail.com>
2023-04-24 18:19:31 -0300

    feat: Adicionando componente BaseRadio

---
+++ frontend/src/components/BaseRadio.vue
@@ -0,0 +1,36 @@
+<template>
+  <label :for="'id_' + value">
+    <input
+      type="radio"
+      :checked="modelValue === value"
+      :value="value"
+      :id="'id_' + value"
+      @change="$emit('update:modelValue', value)"
+      v-bind="$attrs"
+    >
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number, Boolean],
+      default: false,
+    },
+    value: {
+      type: [String, Number, Boolean],
+      required: true,
+    },
+  },
+};
+</script>
