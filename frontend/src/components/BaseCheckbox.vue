df58bc89457293577e4cfb5aed4efbd2d708d4a2
gabrieldeori <deori.pessoal@gmail.com>
2023-04-24 18:05:30 -0300

    feat: Adicionando componente BaseCheckbox

---
+++ frontend/src/components/BaseCheckbox.vue
@@ -0,0 +1,31 @@
+<template>
+  <label :for="'id_' + name">
+    <input
+      type="checkbox"
+      :checked="modelValue"
+      @change="$emit('update:modelValue', $event.target.checked)"
+      :id="'id_' + name"
+      :name="name"
+    >
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
