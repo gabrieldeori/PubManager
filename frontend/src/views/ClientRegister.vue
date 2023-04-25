72f96cd82f0c6902cd3fbfe4ce97755e33eaff5f
gabrieldeori <deori.pessoal@gmail.com>
2023-04-25 13:18:09 -0300

    fix: CRLF lines

---
+++ frontend/src/views/ClientRegister.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <BasicInput
+      v-model="name"
+      label="Name"
+      placeholder="Enter your name"
+      required
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ClientRegister",
+  components: {
+    BasicInput,
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
+</script>
