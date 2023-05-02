901198befdf59e8f9a7e6205fdff99717a4492ab
gabrieldeori <deori.pessoal@gmail.com>
2023-05-02 11:13:28 -0300

    feat: adicionando rota post login com AuthController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AvaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,15 +18,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+use App\Http\Controllers\AuthController;
+Route::post('/login', [AuthController::class, 'login']);
+
+use App\Http\Controllers\AvaController;
+Route::get('/users/show', [AvaController::class, 'getUsers']);
+Route::get('/user', [AvaController::class, 'getAUser']);
+Route::put('/user/edit', [AvaController::class, 'editAUser']);
+Route::post('/user/register', [AvaController::class, 'createUser']);
+Route::delete('/user/delete', [AvaController::class, 'deleteAUser']);
+
 use App\Http\Controllers\ClientsController;
 Route::get('/clients/show', [ClientsController::class, 'getClients']);
 Route::get('/client', [ClientsController::class, 'getAClient']);
 Route::put('/client/edit', [ClientsController::class, 'editAClient']);
 Route::post('/client/register', [ClientsController::class, 'createClients']);
 Route::delete('/client/delete', [ClientsController::class, 'deleteAClient']);
-
-Route::get('/users/show', [AvaController::class, 'getUsers']);
-Route::get('/user', [AvaController::class, 'getAUser']);
-Route::put('/user/edit', [AvaController::class, 'editAUser']);
-Route::post('/user/register', [AvaController::class, 'createUser']);
-Route::delete('/user/delete', [AvaController::class, 'deleteAUser']);
