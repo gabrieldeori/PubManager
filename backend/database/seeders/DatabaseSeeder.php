29545b612b344e970ca4b6a3c916c18c42d5100c
gabrieldeori <deori.pessoal@gmail.com>
2023-05-02 18:50:14 -0300

    feat: Seeder Factory Clients

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Client;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Client::factory(10)->create();
+
         $this->call([
             RootUserSeeder::class,
             ProductsSeeder::class,
