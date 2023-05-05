b957b9ae0fef0b8a6ddb26e5be89750330bfda21
gabrieldeori <deori.pessoal@gmail.com>
2023-05-05 17:09:49 -0300

    feat: readme e lint.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-template-curly-in-string': 'off'
+    'no-template-curly-in-string': 'off',
+    "no-alert": "off"
   },
   overrides: [
     {
